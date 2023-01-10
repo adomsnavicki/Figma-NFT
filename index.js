3bf8ac36a64ee1d4706dc415b85fddae1a4c4019
AdomsNavicki <adomsnavicki@gmail.com>
2023-01-10 14:13:32 +0200

    java-commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var color = ["#d224ff", "#FFFFFF"];
 var i = 0;
 document.querySelector("button").addEventListener("click", function () {
-  i = i < color.length ? ++i : 0;
+  i = i < color.length ? +i : 0;
   document.querySelector(".container").style.background = color[i];
 });
