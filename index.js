222793eb24f0f9d9e105c7847a275587140c7761
AdomsNavicki <adomsnavicki@gmail.com>
2023-01-13 09:27:52 +0200

    dark-mode-button

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-var color = ["#d224ff", "#FFFFFF"];
-var i = 0;
-document.querySelector(".btn-color").addEventListener("click", function () {
-  i = i < color.length ? +i : 0;
-  document.querySelector(".container").style.background = color[i];
-});
+var dark = document.getElementById("dark");
+
+dark.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+};
