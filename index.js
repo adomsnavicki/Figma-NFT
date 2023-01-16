830e1a1a7ae63de210f7f937285aeb15122a7590
AdomsNavicki <adomsnavicki@gmail.com>
2023-01-16 23:59:24 +0200

    update-animation

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ var dark = document.getElementById("dark");
 dark.onclick = function () {
   document.body.classList.toggle("dark-theme");
 };
+
+var test = document.getElementById("test");
+
+test.onclick = function () {
+  document.body.classList.toggle("test");
+  
+};
